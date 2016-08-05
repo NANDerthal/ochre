6e054717115cecd67bdd5d49690535844a786d37
jeff souza <jeffreymsouza@gmail.com>
2016-08-05 17:28:32 -0600

    Add pip and pip3

--- .before_install_linux.sh
+++ .before_install_linux.sh
@@ -1,5 +1,9 @@
 sudo apt-add-repository -y ppa:cython-dev/master-ppa
 sudo apt-add-repository -y ppa:zoogie/sdl2-snapshots
 sudo apt-get update
+
+curl https://bootstrap.pypa.io/get-pip.py | sudo python
+curl https://bootstrap.pypa.io/get-pip.py | sudo python3
+
 export PATH=~/usr/bin:$PATH
 
