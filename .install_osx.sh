195179c5e07306e5d9940cb9e3b7f02ebbf36f70
jeff souza <jeffreymsouza@gmail.com>
2016-08-05 15:33:20 -0600

    install libSOIL on osx

--- .install_osx.sh
+++ .install_osx.sh
@@ -2,7 +2,10 @@ brew install sdl2
 brew install sdl2_ttf
 brew install glew
 brew install glm
-# TODO: install SOIL
+
+git clone https://github.com/smibarber/libSOIL.git
+cd libSOIL; make; make install; cd ..;
+
 brew install python3
 virtualenv venv -p python3
 source venv/bin/activate
