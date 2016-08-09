3d4d63d04be3e382387113240f435668333279d3
Sunny <quatridunull@gmail.com>
2016-08-09 13:58:32 -0600

    use pyvenv

--- .install_linux.sh
+++ .install_linux.sh
@@ -5,6 +5,6 @@ sudo apt-get --force-yes -y install libsdl2-dev
 sudo apt-get --force-yes -y install libsdl2-ttf-dev
 export PYTHON=$TRAVIS_PYTHON_VERSION
 # make new virtualenv
-virtualenv --system-site-packages ~/testvenv
-source ~/testvenv/bin/activate
+pyvenv -p python$PYTHON --system-site-packages testvenv
+source testvenv/bin/activate
 
