1b353c715df923e07b9a52c46daad6b104b0c6e5
Sunny <quatridunull@gmail.com>
2016-08-09 11:53:34 -0600

    Add ensurepip

--- .install_python.sh
+++ .install_python.sh
@@ -1,4 +1,5 @@
 curl https://bootstrap.pypa.io/get-pip.py | sudo -H python$PYTHON
-sudo -H python$PYTHON -m python$PYTHON install cython
-sudo -H python$PYTHON -m pip install -r requirements.txt
+python$PYTHON -m ensurepip
+sudo -H pip$PYTHON install cython
+sudo -H pip$PYTHON install -r requirements.txt
 
