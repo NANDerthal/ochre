742ef915d14de7e0949a724ba2871f62c294bc3b
Sunny <quatridunull@gmail.com>
2016-08-11 18:03:30 -0400

    Do not create virtual environment in linux

--- .install_linux.sh
+++ .install_linux.sh
@@ -5,14 +5,14 @@ sudo apt-get --force-yes -y install libsdl2-dev
 sudo apt-get --force-yes -y install libsdl2-ttf-dev
 export PYTHON=$TRAVIS_PYTHON_VERSION
 # make new virtualenv
-virtualenv·-p·python$PYTHON ./venv
+#virtualenv·-p·python$PYTHON ./venv
 #virtualenv --system-site-packages ./venv
-echo "ls"
-ls
-echo "ls venv"
-ls venv
-echo "ls venv/bin"
-ls venv/bin
-. ./venv/bin/activate
-export PYTHON=$TRAVIS_PYTHON_VERSION
+#echo "ls"
+#ls
+#echo "ls venv"
+#ls venv
+#echo "ls venv/bin"
+#ls venv/bin
+#. ./venv/bin/activate
+#export PYTHON=$TRAVIS_PYTHON_VERSION
 
