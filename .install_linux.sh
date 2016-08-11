a8c1ebe35efa9081ae29e78c19d991497ff771ba
Sunny <quatridunull@gmail.com>
2016-08-11 17:57:27 -0400

    Change order of virtuanenv commands

--- .install_linux.sh
+++ .install_linux.sh
@@ -5,7 +5,7 @@ sudo apt-get --force-yes -y install libsdl2-dev
 sudo apt-get --force-yes -y install libsdl2-ttf-dev
 export PYTHON=$TRAVIS_PYTHON_VERSION
 # make new virtualenv
-virtualenv·venv·-p·python$PYTHON
+virtualenv·-p·python$PYTHON ./venv
 #virtualenv --system-site-packages ./venv
 echo "ls"
 ls
