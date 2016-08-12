3c30b314677d9f45b046e4e078abdcf46c90c22b
Sunny <quatridunull@gmail.com>
2016-08-12 14:37:43 -0400

    Delete unused script

--- .install_python.sh
+++
@@ -1,7 +0,0 @@
-echo $TRAVIS_PYTHON_VERSION
-python --version
-curl https://bootstrap.pypa.io/get-pip.py | sudo -H python
-pip --version
-sudo -H python -m pip install cython
-sudo -H python -m pip install -r requirements.txt
-
